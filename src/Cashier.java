ccd3899855da6f1553234cfd5b87a7269ebbc349
corpuzz <corpuz.markjanlee@gmail.com>
2023-10-14 18:52:02 +0800

    Cashier class done

--- src/Cashier.java
+++ src/Cashier.java
@@ -4,18 +4,19 @@ public class Cashier extends Employee {
     private String branch;
     private double rate;
 
-    public void  setBranch(String branch){
-        this.branch = branch;
-    }
+    @Override
     public void setRate(){
         Scanner input =new Scanner(System.in);
         System.out.println("Enter rate/day: ");
         rate = input.nextDouble();
     }
-    public  String getBranch(){
-        return branch;
-    }
     public double getRate(){
         return  rate;
+    }
+    public void  setBranch(String branch){
+        this.branch = branch;
+    }
+    public  String getBranch(){
+        return branch;
     }
 }
--- src/CreateEmployee.java
+++ src/CreateEmployee.java
@@ -1,10 +1,13 @@
 public class CreateEmployee {
     public static void main(String[] args) {
         Cashier cashier1 = new Cashier();
-        cashier1.setName("Juan Dela Cruz");
+        cashier1.setName("Mekus Insan");
         cashier1.setBranch("Baguio");
         cashier1.setType('p');
 
         System.out.println(cashier1.getName() + " at " + cashier1.getBranch() + " branch : ₱" + cashier1.getSalary());
+
+
+
     }
 }
